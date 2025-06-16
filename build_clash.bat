92ec3859974ea79f843a8257e540f5fab812ae00
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-16 16:51:42 -0400

    Update build_clash.bat

--- build_clash.bat
+++ build_clash.bat
@@ -2,3 +2,9 @@
 nasm -f win32 clash.asm -o clash.obj
 GoLink clash.obj kernel32.dll
 echo Clash Executable Ready: clash.exe
+
+@echo off
+nasm -f win32 clash_bootstrap.asm -o clash.obj
+GoLink clash.obj kernel32.dll
+echo Clash Compiler Ready! Output: clash.exe
+
