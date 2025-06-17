71e7c24282ea6eb0543b4743915018117966ec7e
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-17 17:01:22 -0400

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -4,3 +4,9 @@ nasm -f win64 output.asm -o output.obj
 GoLink /console /entry _start output.obj
 echo Done. Running output.exe...
 output.exe
+
+@echo off
+python clashc.py input.clsh output.asm
+nasm -f win64 output.asm -o output.obj
+GoLink /console /entry _start output.obj
+output.exe
