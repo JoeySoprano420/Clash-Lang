5289f4c438ed99fc106180f0fb2e68e963889b0a
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-17 17:00:56 -0400

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -5,3 +5,10 @@ ld output.o -o output
 chmod +x output
 echo "🎉 Built Linux binary: ./output"
 ./output
+
+#!/bin/bash
+python3 clashc.py input.clsh output.asm
+nasm -f elf64 output.asm -o output.o
+ld output.o -o output
+chmod +x output
+./output
