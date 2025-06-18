fcbaa4fad315306ffc8070b28045e2763712974f
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-18 05:15:26 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,3 +2,9 @@ python clashc.py input.clsh output.asm
 nasm -f elf64 output.asm -o output.o
 ld output.o -o output
 ./output
+
+python clashc.py input.clsh
+nasm -f elf64 input.asm -o input.o
+nasm -f elf64 System.asm -o System.o
+ld input.o System.o -o output
+./output
